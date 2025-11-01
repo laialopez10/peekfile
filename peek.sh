f03eeac2cb92b80648bc165866b5c3b3006d1fc1
laialopez10 <laialsantaa@gmail.com>
2025-11-01 18:16:39 +0100

    Update peek.sh: show full content if file has ≤2X lines, otherwise print warning and first/last X lines

--- peek.sh
+++ peek.sh
@@ -1,3 +1,12 @@
-head -n ${2:-3} $1
-echo "..."
-tail -n ${2:-3} $1
+file="$1"
+lines="${2:-3}"
+total_lines=$(wc -l < "$file")
+
+if [[ $total_lines -le $(( 2 * lines)) ]]; then 
+   cat "$file"; 
+else
+   echo "Warning! The file $file has more than $(( 2 * lines)) lines."
+   head -n $lines $file
+   echo "..."
+   tail -n $lines $file
+fi
