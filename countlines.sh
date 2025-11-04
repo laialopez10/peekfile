c97431c29b44f7a359f2bb07efee32e90b89cde7
laialopez10 <laialsantaa@gmail.com>
2025-11-04 20:55:30 +0100

    Count lines in multiple files

--- countlines.sh
+++ countlines.sh
@@ -1,11 +1,12 @@
-file=$1
-num_lines=$(wc -l < $file)
+for file in $@; do
+    num_lines=$(wc -l < $file)
 
-if [[ $num_lines -eq 0 ]]; then
-	echo The file has 0 lines
-elif [[ $num_lines -eq 1 ]]; then
-	echo The file has 1 line
-else 
-	echo The file has $num_lines lines
-fi
+    if [[ $num_lines -eq 0 ]]; then
+	  echo The file has 0 lines
+    elif [[ $num_lines -eq 1 ]]; then
+	  echo The file has 1 line
+    else 
+	  echo The file has $num_lines lines
+    fi
+done
 
